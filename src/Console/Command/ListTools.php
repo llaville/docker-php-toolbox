5d5dad33d17fe5f532fb25457a30ea329a38ab32
Laurent Laville <laurent.laville@gmail.com>
2021-08-05 05:11:58 +0000

    add import missing

--- src/Console/Command/ListTools.php
+++ src/Console/Command/ListTools.php
@@ -12,6 +12,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+use function array_intersect;
 use function count;
 use function in_array;
 use function is_dir;
